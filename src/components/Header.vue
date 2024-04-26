98a60787b8af2ebcd3ec4f4fe707149e7e315edb
Ahmed Elsayed <me@ahmedelsayed.me>
2024-04-26 09:16:20 +0300

    Finish Task

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,7 +2,7 @@
    <header>
     <div class="lg:flex lg:items-center lg:justify-between h-[80px] px-[48px] mb-[12px]">
       <div class="min-w-0 flex-1">
-        <h2 class="text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight">List Posts</h2>
+        <h2 class="text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight">Posts</h2>
       </div>
     </div>
   </header>
